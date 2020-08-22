aecc145139cf3fc70c377514e48d7631f53c0ca2
Joey ter Meulen <joeytermeulen@gmail.com>
2020-08-22 12:59:06 +0200

    remove css files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
   }
+`;
+
+const AppContainer = styled.p`
+  text-align: center;
+`;
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      <AppContainer>
+        testing
+      </AppContainer>
+    </React.Fragment>
+  )
 }
 
 export default App;
